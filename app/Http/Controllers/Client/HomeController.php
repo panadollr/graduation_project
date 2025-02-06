9271990534eb38a4f806c2f07933d02c88c2b0e0
lank2 <lanvkuk2@gmail.com>
2025-02-06 18:30:00 +0700

    update

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -13,55 +13,57 @@ class HomeController extends Controller
     protected $resourceDir = 'client.home';
     public function index()
     {
-        $sliders = Slider::select(['image', 'link_url'])->get();
-        $categories = Category::with([
-            'products' => function ($query) {
-                $query->with('reviews')
-                    ->withCount(['reviews' => function ($query) {
-                        $query->whereNull('parent_id');
-                    }]);
-            }
-        ])
-            ->get();
+        // $sliders = Slider::select(['image', 'link_url'])->get();
+        // $categories = Category::with([
+        //     'products' => function ($query) {
+        //         $query->with('reviews')
+        //             ->withCount(['reviews' => function ($query) {
+        //                 $query->whereNull('parent_id');
+        //             }]);
+        //     }
+        // ])
+        //     ->get();
 
-        $popularCategories = Category::with([
-            'products' => function ($query) {
-                $query->with('reviews')
-                    ->withCount(['reviews' => function ($query) {
-                        $query->whereNull('parent_id');
-                    }]);
-            }
-        ])
-            ->whereNull('parent_id')
-            ->take(6)
-            ->get();
+        // $popularCategories = Category::with([
+        //     'products' => function ($query) {
+        //         $query->with('reviews')
+        //             ->withCount(['reviews' => function ($query) {
+        //                 $query->whereNull('parent_id');
+        //             }]);
+        //     }
+        // ])
+        //     ->whereNull('parent_id')
+        //     ->take(6)
+        //     ->get();
 
-        // Lấy sản phẩm đang giảm giá
-        $saleProducts = Product::where('discount_percentage', '>', 0)
-            ->with(['category:id,name', 'reviews'])
-            ->withCount(['reviews as reviews_count' => function ($query) {
-                $query->whereNull('parent_id');
-            }])
-            ->orderByDesc('discount_percentage')
-            ->take(10)
-            ->get();
+        // // Lấy sản phẩm đang giảm giá
+        // $saleProducts = Product::where('discount_percentage', '>', 0)
+        //     ->with(['category:id,name', 'reviews'])
+        //     ->withCount(['reviews as reviews_count' => function ($query) {
+        //         $query->whereNull('parent_id');
+        //     }])
+        //     ->orderByDesc('discount_percentage')
+        //     ->take(10)
+        //     ->get();
 
 
-        $categoriesOfSaleProducts = $saleProducts->pluck('category')->unique();
+        // $categoriesOfSaleProducts = $saleProducts->pluck('category')->unique();
 
-        $blogs = Blog::select('title', 'slug', 'image', 'created_at', 'category_id')
-            ->with(['category:id,name'])
-            ->take(12)
-            ->latest()
-            ->get();
+        // $blogs = Blog::select('title', 'slug', 'image', 'created_at', 'category_id')
+        //     ->with(['category:id,name'])
+        //     ->take(12)
+        //     ->latest()
+        //     ->get();
 
-        return view($this->resourceDir . '.index', compact(
-            'sliders',
-            'categories',
-            'popularCategories',
-            'categoriesOfSaleProducts',
-            'blogs',
-            'saleProducts',
-        ));
+        // return view($this->resourceDir . '.index', compact(
+        //     'sliders',
+        //     'categories',
+        //     'popularCategories',
+        //     'categoriesOfSaleProducts',
+        //     'blogs',
+        //     'saleProducts',
+        // ));
+
+        return 'Hello World';
     }
 }
