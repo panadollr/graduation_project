0b1aeeff22bfa6a50e0715817fb7d2b48efb1dfb
panadollr <cupleofgamer@gmail.com>
2025-02-08 21:45:19 +0700

    update

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -1,81 +1,85 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Admin' }}</title>
     <!-- Thêm CSS, meta tags, etc. -->
-    <meta name="csrf_token" value="{{ csrf_token() }}"/>
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/feather/feather.css')}}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/mdi/css/materialdesignicons.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/ti-icons/css/themify-icons.css')}}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/font-awesome/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/typicons/typicons.css')}}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/simple-line-icons/css/simple-line-icons.css')}}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/css/vendor.bundle.base.css')}}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css')}}">
+    <meta name="csrf_token" value="{{ csrf_token() }}" />
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/feather/feather.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/typicons/typicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/simple-line-icons/css/simple-line-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/css/vendor.bundle.base.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('admin/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css') }}">
     <!-- endinject -->
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('admin/assets/js/select.dataTables.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/assets/js/select.dataTables.min.css') }}">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{ secure_asset('admin/assets/css/style.css') }}">
     <!-- endinject -->
-    <link rel="shortcut icon" href="{{ asset('client/assets/images/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ secure_asset('client/assets/images/favicon.png') }}" />
     @livewireStyles
 </head>
+
 <body class="with-welcome-text">
 
     @include('layouts.toast')
-    
-    @if(request()->route()->getName() != 'admin.login')
-    <div class="container-scroller">
-        <!-- partial:partials/_navbar.html -->
-        @include('admin.partials.navbar')
-        <!-- partial -->
-        <div class="container-fluid page-body-wrapper">
-          <!-- partial:partials/_sidebar.html -->
-          @include('admin.partials.sidebar')
-          <!-- partial -->
-          <div class="main-panel">
-              <div class="content-wrapper">
-                @yield('content') <!-- Nơi để các trang con hiển thị nội dung -->
-                <!-- content-wrapper ends -->
-          <!-- partial:partials/_footer.html -->
-          @include('admin.partials.footer')
-          <!-- partial -->
+
+    @if (request()->route()->getName() != 'admin.login')
+        <div class="container-scroller">
+            <!-- partial:partials/_navbar.html -->
+            @include('admin.partials.navbar')
+            <!-- partial -->
+            <div class="container-fluid page-body-wrapper">
+                <!-- partial:partials/_sidebar.html -->
+                @include('admin.partials.sidebar')
+                <!-- partial -->
+                <div class="main-panel">
+                    <div class="content-wrapper">
+                        @yield('content') <!-- Nơi để các trang con hiển thị nội dung -->
+                        <!-- content-wrapper ends -->
+                        <!-- partial:partials/_footer.html -->
+                        @include('admin.partials.footer')
+                        <!-- partial -->
+                    </div>
+                    <!-- main-panel ends -->
+                </div>
+                <!-- page-body-wrapper ends -->
+            </div>
         </div>
-        <!-- main-panel ends -->
-      </div>
-      <!-- page-body-wrapper ends -->
-    </div>
-    </div>
     @else
-      @yield('content')
+        @yield('content')
     @endif
-    
+
     <!-- plugins:js -->
-    <script src="{{ asset('admin/assets/vendors/js/vendor.bundle.base.js')}}"></script>
-    <script src="{{ asset('admin/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
+    <script src="{{ asset('admin/assets/vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('admin/assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
     <!-- endinject -->
     <!-- Plugin js for this page -->
-    <script src="{{ asset('admin/assets/vendors/chart.js/chart.umd.js')}}"></script>
-    <script src="{{ asset('admin/assets/vendors/progressbar.js/progressbar.min.js')}}"></script>
+    <script src="{{ asset('admin/assets/vendors/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('admin/assets/vendors/progressbar.js/progressbar.min.js') }}"></script>
     <!-- End plugin js for this page -->
     <!-- inject:js -->
-    <script src="{{ asset('admin/assets/js/off-canvas.js')}}"></script>
+    <script src="{{ asset('admin/assets/js/off-canvas.js') }}"></script>
     {{-- <script src="{{ asset('admin/assets/js/template.js')}}"></script> --}}
-    <script src="{{ asset('admin/assets/js/settings.js')}}"></script>
-    <script src="{{ asset('admin/assets/js/hoverable-collapse.js')}}"></script>
-    <script src="{{ asset('admin/assets/js/todolist.js')}}"></script>
+    <script src="{{ asset('admin/assets/js/settings.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/todolist.js') }}"></script>
     <!-- endinject -->
     <!-- Custom js for this page-->
-    <script src="{{ asset('admin/assets/js/jquery.cookie.js')}}" type="text/javascript"></script>
-    <script src="{{ asset('admin/assets/js/dashboard.js')}}"></script>
+    <script src="{{ asset('admin/assets/js/jquery.cookie.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('admin/assets/js/dashboard.js') }}"></script>
     <!-- <script src="assets/js/Chart.roundedBarCharts.js"></script> -->
     <!-- End custom js for this page-->
     @livewireScripts
     @yield('script')
 </body>
+
 </html>
