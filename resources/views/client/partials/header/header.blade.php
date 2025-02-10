a2b430b7a918056506069c75512a01a0550f7268
panadollr <cupleofgamer@gmail.com>
2025-02-10 20:44:16 +0700

    update

--- resources/views/client/partials/header/header.blade.php
+++ resources/views/client/partials/header/header.blade.php
@@ -6,19 +6,18 @@
                     <span class="sr-only">Toggle mobile menu</span>
                     <i class="icon-bars"></i>
                 </button>
-                
+
                 <a href="{{ route('home') }}" class="logo">
-                    <img src="{{ asset('client/assets/images/logo.png') }}" alt="" width="105" height="25">
+                    <img src="{{ asset('client/assets/images/logo.png') }}" alt="" width="105"
+                        height="25">
                 </a>
 
                 <div>
-                    @include('client.partials.header.category-dropdown')
+                    {{-- @include('client.partials.header.category-dropdown') --}}
                 </div>
 
-                {{-- @livewire('client.chatbot') --}}
-
             </div><!-- End .header-left -->
-            
+
 
             <div class="header-center">
                 @livewire('client.product-search')
@@ -27,16 +26,17 @@
             <div class="header-right">
 
                 <div class="account">
-                    <a @auth 
-                        href="{{ route('account.index') }}" title="Tài khoản"
+                    <a
+                        @auth href="{{ route('account.index') }}" title="Tài khoản"
                        @else
-                       href="#signin-modal" data-toggle="modal"
-                       @endauth
-                    >
+                       href="#signin-modal" data-toggle="modal" @endauth>
                         <div class="icon">
                             <i class="icon-user"></i>
                         </div>
-                        <p style='font-family: sans-serif;'>@auth Tài khoản của tôi @else Đăng nhập @endauth</p>
+                        <p style='font-family: sans-serif;'>@auth Tài khoản của tôi
+                            @else
+                            Đăng nhập @endauth
+                        </p>
                     </a>
                 </div>
 
@@ -48,5 +48,5 @@
         </div><!-- End .container -->
     </div><!-- End .header-middle -->
 
- 
+
 </header><!-- End .header -->
